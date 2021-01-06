4a607c316165f6b5bba5032e269c21a3cb817aa2
“Fangyu-Zhou” <fangyu22@ufl.edu>
2021-01-06 18:56:18 -0500

    added react router dom and added router for hats page(temp)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import './App.css';
+import {Route} from 'react-router-dom'
 
 import Homepage from "./pages/homepage/homepage";
 
+import './App.css';
+
 function App() {
-  return (
-    <div>
-      <Homepage></Homepage>
-    </div>
-  );
+    return (
+        <div>
+            <Route exact path='/' component={Homepage} />
+            <Route exact path='/hats' component={Hats} />
+        </div>
+    );
+}
+
+function Hats() {
+    return (
+        <div>Hats page</div>
+    )
 }
 
 export default App;
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'
 
 import './menu-item.scss'
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`menu-item ${size}`}>
+const MenuItem = ({title, imageUrl, size, url, history, match}) => (
+    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${url}`)}>
         <div
             className={`background-image`}
             style={{
@@ -13,9 +14,9 @@ const MenuItem = ({title, imageUrl, size}) => (
         </div>
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
-            <h2 className='subtitle'>SHop Now</h2>
+            <h2 className='subtitle'>Shop Now</h2>
         </div>
     </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
