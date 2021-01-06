249178307344c3f7e90ffb4c9795181df2f33aee
“Fangyu-Zhou” <fangyu22@ufl.edu>
2021-01-06 16:39:51 -0500

    created app and add hompage files

---
+++ src/components/homepage/homepage.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './homepage.scss'
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>title</h1>
+                    <h2 className='subtitle'>subtitle</h2>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>title</h1>
+                    <h2 className='subtitle'>subtitle</h2>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>title</h1>
+                    <h2 className='subtitle'>subtitle</h2>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>title</h1>
+                    <h2 className='subtitle'>subtitle</h2>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>title</h1>
+                    <h2 className='subtitle'>subtitle</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage
