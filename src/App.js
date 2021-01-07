75a6bf4d5eb436242337c0a0c1bbbee36bf808b5
“Fangyu-Zhou” <fangyu22@ufl.edu>
2021-01-07 17:58:58 -0500

    added header part for routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 import Homepage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shopPage";
+import Header from "./components/header/header";
 
 import './App.css';
 
 function App() {
     return (
         <div>
-            <Route exact path='/' component={Homepage} />
-            <Route exact path='/shop' component={ShopPage} />
+            <Header />
+            <Switch>
+                <Route exact path='/' component={Homepage}/>
+                <Route exact path='/shop' component={ShopPage}/>
+            </Switch>
         </div>
     );
 }
