20f5fc4437e31afba213925b857996d1573c3a70
“Fangyu-Zhou” <fangyu22@ufl.edu>
2021-01-10 21:41:07 -0500

    added signup component to sign-in page, styled sign up component and enabled signup function in codebase and firestore auth

--- src/pages/sign-in-and-up-page/sign-in-page.jsx
+++ src/pages/sign-in-and-up-page/sign-in-page.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in'
+import SignUp from "../../components/sign-up/sign-up";
+
+
 import './sign-in-page.scss';
 
 const SignInPage = () => (
-    <div className='sign-in'>
+    <div className='sign-in-page'>
         <SignIn />
+        <SignUp />
     </div>
 );
 export default SignInPage;
