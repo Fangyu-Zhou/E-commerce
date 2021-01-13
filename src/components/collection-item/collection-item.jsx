7847760e76db266e559c8eccd1dd36294a88c202
“Fangyu-Zhou” <fangyu22@ufl.edu>
2021-01-12 22:26:17 -0500

    added ADD_ITEM action to cart reducer and integrate into collection-item component, when click on ADD_TO_CART, ADD_ITEM action is triggered

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import CustomButton from "../wigets/custom-button/custom-button";
 
+import {addItem} from "../../redux/cart/cart.action";
+
 import './collection-item.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className='collection-item'>
-        <div
-            className='image'
-            style={{backgroundImage: `url(${imageUrl})`}}
-        />
+const CollectionItem = ({item, addItem}) => {
+    const {imageUrl, name, price} = item;
 
-        <div className='collection-footer'>
-            <div className='name'>{name}</div>
-            <div className='price'>${price}</div>
+    return (
+        <div className='collection-item'>
+            <div
+                className='image'
+                style={{backgroundImage: `url(${imageUrl})`}}
+            />
+
+            <div className='collection-footer'>
+                <div className='name'>{name}</div>
+                <div className='price'>${price}</div>
+            </div>
+
+            <CustomButton onClick={() => addItem(item)} inverted>
+                ADD TO CART
+            </CustomButton>
         </div>
+    );
+}
 
-        <CustomButton inverted>ADD TO CART</CustomButton>
-    </div>
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
 
-);
-
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import {cartActionType} from "./cart.actionType";
 
 const INITIAL_STATE = {
-    hiddenCartDropDown: true
+    hiddenCartDropDown: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +12,14 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 hiddenCartDropDown: !state.hiddenCartDropDown
             };
+        case cartActionType.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [
+                    ...state.cartItems,
+                    action.payload
+                ]
+            }
         default:
             return state;
     }
