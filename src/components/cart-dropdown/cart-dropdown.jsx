5226e090b1493fd12d2982af4b82afc84ee56875
“Fangyu-Zhou” <fangyu22@ufl.edu>
2021-01-13 21:40:30 -0500

    added reselect library and use selectors for cartIcon and dropdown component to avoid unnecessary rerendering

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import {selectCartItems} from "../../redux/cart/cart.selectors";
+
 import CustomButton from "../wigets/custom-button/custom-button";
 import CartItem from "../cart-item/cart-item";
 
@@ -20,8 +22,8 @@ const CartDropDown = ({cartItems}) => (
 
 );
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropDown);
