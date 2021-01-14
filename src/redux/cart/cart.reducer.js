48ed1dffb5cba00b141dae63ce36ab85f3d82a7b
“Fangyu-Zhou” <fangyu22@ufl.edu>
2021-01-13 20:37:42 -0500

    added items grouping functionality for carts, updated shopPageData with unique ids for items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import {cartActionType} from "./cart.actionType";
+import {addItemToCart} from "./cart.utils";
 
 const INITIAL_STATE = {
     hiddenCartDropDown: true,
@@ -15,10 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case cartActionType.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [
-                    ...state.cartItems,
-                    action.payload
-                ]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state;
