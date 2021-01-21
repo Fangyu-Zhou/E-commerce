2d3a4d860c2f4e1302aa66628ec30f6eb0174d36
“Fangyu-Zhou” <fangyu22@ufl.edu>
2021-01-20 22:29:19 -0500

    added redux persist lib and persist cart states into local storage

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import App from './App';
 
-import store from "./redux/store";
+import {store, persistor} from "./redux/store";
 
 import './index.css';
 
@@ -14,7 +15,9 @@ ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+                <PersistGate persistor={persistor}>
+                    <App />
+                </PersistGate>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
